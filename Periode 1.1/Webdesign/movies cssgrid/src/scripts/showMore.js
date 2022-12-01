ee325cfbc6969735a2d3420be2059558c07c36f0
Sno3t <tycho.brakenhoff@ictmbo.nl>
2022-12-01 12:02:35 +0100

    Added bootstrap and cssgrid website

---
+++ Periode 1.1/Webdesign/movies cssgrid/src/scripts/showMore.js
@@ -0,0 +1,24 @@
+// function myFunction() {
+//     var dots = document.getElementById("dots");
+//     var moreText = document.getElementById("more");
+//     var btnText = document.getElementById("myBtn");
+//
+//     if (dots.style.display === "none") {
+//         dots.style.display = "inline";
+//         btnText.innerHTML = "Read more";
+//         moreText.style.display = "none";
+//     } else {
+//         dots.style.display = "none";
+//         btnText.innerHTML = "Read less";
+//         moreText.style.display = "inline";
+//     }
+// }
+
+function myFunction() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
